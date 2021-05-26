97a46e4f873d6774c15b8861f7dff0621b3520f2
KauanRakoski <kauan.k.rakoski@gmail.com>
2021-05-26 00:28:10 +0000

    add emailjs

--- js/all.js
+++ js/all.js
@@ -2,48 +2,70 @@ gsap.registerPlugin(ScrollTrigger)
 
 // Fade-in images
 gsap.utils.toArray(".fade-in").forEach(fader => {
-  var tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: fader,
-      start: "top 95%",
-    }
-  });
+    var tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: fader,
+            start: "top 95%",
+        }
+    });
 
-  tl.from(fader, {
-    duration: 2,
-    opacity: 0
-  });
+    tl.from(fader, {
+        duration: 2,
+        opacity: 0
+    });
 });
 
 // Trigger for the text that comes from left
 gsap.utils.toArray(".come-left").forEach(lefty => {
-  var tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: lefty,
-      start: "top 95%",
-    }
-  });
+    var tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: lefty,
+            start: "top 95%",
+        }
+    });
 
-  tl.from(lefty, {
-    duration: 1,
-     opacity: 0, 
-     x: -200,
-  });
+    tl.from(lefty, {
+        duration: 1,
+        opacity: 0,
+        x: -200,
+    });
 });
 
 // Trigger for the text that comes from right
 gsap.utils.toArray(".come-right").forEach(righty => {
-  var tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: righty,
-      start: "top 95%",
-    }
-  });
+    var tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: righty,
+            start: "top 95%",
+        }
+    });
 
-  tl.from(righty, {
-    duration: 1,
-     opacity: 0, 
-     x: 200,
-  });
+    tl.from(righty, {
+        duration: 1,
+        opacity: 0,
+        x: 200,
+    });
 });
 
+
+
+// Email JS
+
+(function () {
+    emailjs.init("user_dPvKSoLrQyL25taFI4O4u");
+})()
+
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // Service ID and template ID
+        emailjs.sendForm('service_ntmuk8d', 'template_pvart32', this)
+            .then(function () {
+                console.log('SUCCESS!');
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
--- js/dist/all.dev.js
+++ js/dist/all.dev.js
@@ -41,4 +41,22 @@ gsap.utils.toArray(".come-right").forEach(function (righty) {
     opacity: 0,
     x: 200
   });
-});
+}); // Email JS
+
+(function () {
+  emailjs.init("user_dPvKSoLrQyL25taFI4O4u");
+})();
+
+window.onload = function () {
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault(); // generate a five digit number for the contact_number variable
+
+    this.contact_number.value = Math.random() * 100000 | 0; // Service ID and template ID
+
+    emailjs.sendForm('service_ntmuk8d', 'template_pvart32', this).then(function () {
+      console.log('SUCCESS!');
+    }, function (error) {
+      console.log('FAILED...', error);
+    });
+  });
+};
